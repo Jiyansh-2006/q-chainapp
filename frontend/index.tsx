95fbb9fec6416522d20ad4f057cea8643eb7cae7
Jiyansh-2006 <jiyanshkushwahmain@gmail.com>
2026-03-01 05:46:19 +0530

    Update index.tsx

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { WalletProvider } from './contexts/WalletContext';
 import { AlgorandWalletProvider } from './contexts/AlgorandWalletContext';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
